009a0c23382d93b8c3ba067e8a4f12fb46911a9a
brian wisnowski <brian wisnowski>
2017-03-22 17:36:46 -0400

    Time to add questions

--- javascript/app.js
+++ javascript/app.js
@@ -5,28 +5,34 @@
 window.onload = function () {
 
 var countdown = document.getElementById('timer')
-var gameOver = document.getElementById('over')
+
 var seconds = 20
 var second = 0
 var interval;
-var images = ["sushi.jpg"]
+
+
+
 
 function displayImage() {
-	
+	$("#outcome").html("giphy.gif");
 }
 
-gameOver.style.display = 'none';
 
 interval = setInterval(function() {
 
 	countdown.innerHTML = '<strong>You have<strong> ' + (seconds - second) + ' seconds left!<strong>';
 	if (second >= seconds) {
 		clearInterval(interval);
-	   
+		$("#outcome").html("<img src='css/giphy.gif' height='300px' width='200px'/>")
+		$("#question").html("Time's Up!!")
+
+	 
+	  
+
 
 	}
 	second++;
 
-}, 1000);
+}, 50);
 
 }
